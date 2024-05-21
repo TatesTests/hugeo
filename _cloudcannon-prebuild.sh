c0208b6224896171d5d2053d14029a66db9b44d2
🐢 Tests <tate+testing@cloudcannon.com>
2024-05-22 11:51:37 +1200

    Updated 1 file via CloudCannon.

--- _cloudcannon-prebuild.sh
+++ _cloudcannon-prebuild.sh
@@ -1,9 +1,1 @@
 #!/bin/bash
-cp siteicon.png static/siteicon.png
-cat ~/.bashrc > ~/.bash_profile
-time /usr/bin/env bash -c exit
-which nvm
-nvm use 20
-
-time source /usr/local/.nvm/nvm.sh
-time . /usr/local/.nvm/nvm.sh
